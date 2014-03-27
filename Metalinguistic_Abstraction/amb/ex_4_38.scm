3f3b59f6e9384a0544b1becfedc4b5d22e237cc9
Hikaru Ojima <ojima.h@gmail.com>
2014-03-27 19:43:26 +0000

    ex_4_38

--- Metalinguistic_Abstraction/amb/ex_4_38.scm
+++ Metalinguistic_Abstraction/amb/ex_4_38.scm
@@ -20,7 +20,7 @@
      (require (not (= fletcher 5)))
      (require (not (= fletcher 1)))
      (require (> miller cooper))
-     (require (not (= (abs (- smith fletcher)) 1)))
+     ;(require (not (= (abs (- smith fletcher)) 1)))
      (require (not (= (abs (- fletcher cooper)) 1)))
      (list (list 'baker baker)
 	   (list 'cooper cooper)
@@ -30,3 +30,35 @@
  (multiple-dwelling))
 
 ;;try-again
+
+;; 5通り
+
+;; <<< (multiple-dwelling)
+;; ;;; Starting a new problem 
+;; ;;; Amb-Eval value:
+;; ((baker 1) (cooper 2) (fletcher 4) (miller 3) (smith 5))
+;; 
+;; ;;; Amb-Eval input:
+;; 
+;; ;;; Amb-Eval value:
+;; ((baker 1) (cooper 2) (fletcher 4) (miller 5) (smith 3))
+;; 
+;; ;;; Amb-Eval input:
+;; 
+;; ;;; Amb-Eval value:
+;; ((baker 1) (cooper 4) (fletcher 2) (miller 5) (smith 3))
+;; 
+;; ;;; Amb-Eval input:
+;; 
+;; ;;; Amb-Eval value:
+;; ((baker 3) (cooper 2) (fletcher 4) (miller 5) (smith 1))
+;; 
+;; ;;; Amb-Eval input:
+;; 
+;; ;;; Amb-Eval value:
+;; ((baker 3) (cooper 4) (fletcher 2) (miller 5) (smith 1))
+;; 
+;; ;;; Amb-Eval input:
+;; 
+;; ;;; There are no more values of
+;; (multiple-dwelling)
